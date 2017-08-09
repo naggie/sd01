8a9326cf73bb1919ca6cc6835057f8700a41b6a7
Callan Bryant <callan.bryant@gmail.com>
2017-08-09 21:28:35 +0100

    add service discovery class

---
+++ sd01.py
@@ -0,0 +1,69 @@
+from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
+from threading import Thread
+from binascii import crc32
+from logging import getLogger
+from time import sleep
+
+log = getLogger(__name__)
+
+try:
+    # Python 3.x
+    from time import monotonic as time
+except ImportError:
+    # Python 2.x, not monotonic
+    from time import time
+
+class Base(Thread):
+    def __init__(self,magic,interval=5):
+        super(Base,self).__init__()
+        self.magic = str(magic).encode('ascii')
+        self.interval = int(interval)
+
+        if self.interval < 1:
+            raise ValueError('Interval must be more than 1')
+
+        # may remove this behavior due to firewall implications
+        # picka deterministic high port number
+        self.port = 10**4 + crc32(self.magic) % 10**4
+
+
+class Announcer(Base):
+    daemon = True
+    def run(self):
+        # create UDP socket
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.bind(('', 0))
+        s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+        while True:
+            log.debug('Announcing on port %s with magic %s',self.port,self.magic)
+            s.sendto(self.magic, ('<broadcast>', self.port))
+            sleep(self.interval)
+
+
+class Discoverer(Base):
+    daemon = True
+    def __init__(self,*args,**kwargs):
+        super(Discoverer,self).__init__(*args,**kwargs)
+
+        # map of host -> timestamp of last announcement
+        self.hosts = dict()
+
+    def run(self):
+        # create UDP socket
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.bind(('', self.port))
+
+        while True:
+            data, addr = s.recvfrom(len(self.magic))
+            if data == self.magic:
+                host = addr[0]
+                log.debug('Discovered %s with magic %s',host,self.magic)
+                self.hosts[host] = time()
+
+    def get_hosts(self,wait=False):
+        if wait:
+            sleep(self.interval+1)
+
+        min_ts = time() - self.interval * 2
+        return [h for h,ts in self.hosts.items() if ts > min_ts]
