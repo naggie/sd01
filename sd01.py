eab6dd0f41be1b40ef92d42e09966833671f7c3b
Callan Bryant <callan.bryant@gmail.com>
2017-08-09 22:48:18 +0100

    pep8

--- sd01.py
+++ sd01.py
@@ -46,12 +46,10 @@ port over 10000. A port can be specified when you have an os-level firewall enab
 
 """
 from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
-from threading import Thread,Lock
+from threading import Thread, Lock
 from binascii import crc32
 from logging import getLogger
 from time import sleep
-
-log = getLogger(__name__)
 
 try:
     # Python 3.x
@@ -60,9 +58,12 @@ except ImportError:
     # Python 2.x, not monotonic
     from time import time
 
+log = getLogger(__name__)
+
+
 class Base(Thread):
-    def __init__(self,magic,interval=5,port=None):
-        super(Base,self).__init__()
+    def __init__(self, magic, interval=5, port=None):
+        super(Base, self).__init__()
         self.magic = str(magic).encode('ascii')
         self.interval = int(interval)
 
@@ -78,6 +79,7 @@ class Base(Thread):
 
 class Announcer(Base):
     daemon = True
+
     def run(self):
         # create UDP socket
         s = socket(AF_INET, SOCK_DGRAM)
@@ -85,15 +87,17 @@ class Announcer(Base):
         s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
         while True:
-            log.debug('Announcing on port %s with magic %s',self.port,self.magic)
+            log.debug('Announcing on port %s with magic %s',
+                      self.port, self.magic)
             s.sendto(self.magic, ('<broadcast>', self.port))
             sleep(self.interval)
 
 
 class Discoverer(Base):
     daemon = True
-    def __init__(self,*args,**kwargs):
-        super(Discoverer,self).__init__(*args,**kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super(Discoverer, self).__init__(*args, **kwargs)
 
         # map of host -> timestamp of last announcement
         self.hosts = dict()
@@ -113,17 +117,18 @@ class Discoverer(Base):
             data, addr = s.recvfrom(len(self.magic))
             if data == self.magic:
                 host = addr[0]
-                log.debug('Discovered %s with magic %s',host,self.magic)
+                log.debug('Discovered %s with magic %s', host, self.magic)
                 self.hosts[host] = time()
 
-    def get_hosts(self,wait=False):
+    def get_hosts(self, wait=False):
         if not self.running:
-            raise RuntimeError('You must call start() first to start listening for announcements')
+            raise RuntimeError(
+                'You must call start() first to start listening for announcements')
 
         if wait:
-            sleep(self.interval+1)
+            sleep(self.interval + 1)
 
         min_ts = time() - self.interval * 2
 
         with self.lock:
-            return [h for h,ts in self.hosts.items() if ts > min_ts]
+            return [h for h, ts in self.hosts.items() if ts > min_ts]
