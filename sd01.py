aa78b250ce68bcff96030c840ee8ad170bcdb0e5
Callan Bryant <callan.bryant@gmail.com>
2017-09-27 23:32:21 +0100

    capitalise my name

--- sd01.py
+++ sd01.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Callan bryant
+# Copyright (c) 2017 Callan Bryant
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
